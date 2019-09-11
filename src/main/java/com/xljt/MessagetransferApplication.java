c0cdeb01b52b522d36285f779987064047e05325
xurao <61674763@qq.com>
2019-09-11 16:34:33 +0800

    车辆信息传输

---
+++ src/main/java/com/xljt/MessagetransferApplication.java
@@ -0,0 +1,13 @@
+package com.xljt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagetransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagetransferApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xljt/domain/Book.java
@@ -0,0 +1,23 @@
+package com.xljt.domain;
+
+public class Book {
+
+    private String name;
+    private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
